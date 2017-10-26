fc05bb78cf60e45d5ada565fb544c246f5275b0c
Alexander <mas_m@ukr.net>
2017-10-27 01:03:23 +0300

    22

---
+++ administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2017-10-26 11:05:14 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2017-10-26T11:05:14+00:00	INFO 127.0.0.1	joomlafailure	Имя пользователя и пароль не совпадают или у вас ещё нет учётной записи на сайте
